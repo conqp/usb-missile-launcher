7f68fa0559af297e7aa4ca98ba8f27ae3f281bf6
Richard Neumann <mail@richard-neumann.de>
2026-04-22 12:55:17 +0200

    Ass OS guard.

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,11 +1,6 @@
 //! TUI controller for the USB missile launcher.
 
 use std::error::Error;
-use std::io::stdout;
-
-use crossterm::event::{KeyboardEnhancementFlags, PushKeyboardEnhancementFlags};
-use crossterm::execute;
-use crossterm::terminal::enable_raw_mode;
 use uml::MissileLauncher;
 
 use self::app::App;
@@ -14,14 +9,30 @@ mod app;
 
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    enable_raw_mode()?;
 
-    execute!(
-        stdout(),
-        PushKeyboardEnhancementFlags(KeyboardEnhancementFlags::REPORT_EVENT_TYPES)
-    )?;
+    #[cfg(not(target_os = "windows"))]
+    enhanced_tty::setup()?;
 
     let missile_launcher = MissileLauncher::open()?;
     ratatui::run(|terminal| App::new(missile_launcher).run(terminal))?;
     Ok(())
+}
+
+
+#[cfg(not(target_os = "windows"))]
+mod enhanced_tty {
+    use std::io::stdout;
+
+    use crossterm::event::{KeyboardEnhancementFlags, PushKeyboardEnhancementFlags};
+    use crossterm::execute;
+    use crossterm::terminal::enable_raw_mode;
+
+    pub fn setup() -> std::io::Result<()> {
+        enable_raw_mode()?;
+
+        execute!(
+            stdout(),
+            PushKeyboardEnhancementFlags(KeyboardEnhancementFlags::REPORT_EVENT_TYPES)
+        )
+    }
 }
